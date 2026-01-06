0540b22e452823a9699fe66845e437caefdbdd9d
clairenoble <c.noble@live.com>
2026-01-06 13:10:13 +0000

    making dcd to aa script more streamlined

--- openawsem/analysis_scripts/dcd_to_aa_pdb.py
+++ openawsem/analysis_scripts/dcd_to_aa_pdb.py
@@ -20,14 +20,16 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("folder", help="The path to the folder with the replica dcd files")
 parser.add_argument("-f", "--fasta", default="./crystal_structure.fasta", help="Reference fasta for PDB mapping. Default is ./crystal_structure.fasta")
+parser.add_argument("-r", "--ref", default="crystal_structure-openmmawsem.pdb")
 parser.add_argument("-dp",
                     "--dcd_pattern",
-                    default="replica*.dcd",
-                    help="pattern to search for DCD files for. Default='replica*.dcd'. Additional '.'s in the name may cause errors")
+                    default="*.dcd",
+                    help="pattern to search for DCD files for. Default='*.dcd'. Additional '.'s in the name may cause errors")
 
 args = parser.parse_args()
 
 folder = args.folder
+ref = args.ref
 
 seq_dic = get_seq_dic(fasta=args.fasta)
 pattern = args.dcd_pattern
@@ -37,7 +39,7 @@ dcd_files = glob.glob(f'{folder}/{pattern}')
 # Replace filenames with your actual files
 for file in dcd_files:
     file_name = file.split('.')[0]
-    traj = md.load(f'{file_name}.dcd', top='crystal_structure-openmmawsem.pdb')
+    traj = md.load(f'{file_name}.dcd', top=ref)
     print(f"Successfully loaded {file_name}.dcd'")
     traj.save_pdb(f"{file_name}_OA.pdb")
     print(f"Wrote coarse-grained PDB {file_name}_OA.pdb")
